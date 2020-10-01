6e11d63e122ce627c71c7150ac4d641a0dd12245
Aki Maja <akmaja@loyola.edu>
2020-09-30 21:50:05 -0400

    game

--- app/src/main/java/com/lukeaki/se_blackjack/GameController.java
+++ app/src/main/java/com/lukeaki/se_blackjack/GameController.java
@@ -8,17 +8,31 @@ import java.util.concurrent.TimeUnit;
 
 public class GameController
 {
+    /**
+    * Initalize user as a player object
+    */
     Player user;
+    /**
+    * Initalize dealer as a player object
+    */
     Player dealer;
+    /**
+    * Initalize a deck to start the game with 
+    */
     Deck deck;
 
+    /**
+    * Constructor method for the game controller
+    */
     public GameController(Player you, Player com)
     {
         user = you;
         dealer = com;
         deck = new Deck();
     }
-
+    /**
+    * Deal method which gives cards to players
+    */
     public void deal()
     {
         // deal 2 cards to player and dealer
@@ -28,6 +42,10 @@ public class GameController
         dealer.newCard(deck);
     }
 
+    /**
+    * Method to receive another card from the deck
+    * @param Player who is hitting  
+    */
     public Card hit(Player hitter)
     {
         // WHEREVER HIT IS CALLED:
@@ -39,7 +57,10 @@ public class GameController
         return drawn;
     }
 
-    // I think this gets called when the stop button is pressed
+    /**
+    * I think this gets called when the stop button is pressed
+    * Method that gets called when the player is done drawing more hards
+    */
     public void playDealer()
     {
         while((dealer.getHandValue() < user.getHandValue()) && (dealer.getHandSize() < 5))
@@ -49,7 +70,10 @@ public class GameController
         checkWinner();
     }
 
-    // returns 0 for regular hand, 1 for blackjack
+    /**
+    * Method which takes the players hand value and sees if they have blackjack
+    *returns 0 for regular hand, 1 for blackjack
+    */
     public int bjTest(Player hand)
     {
         int bjack = 0;
@@ -61,8 +85,10 @@ public class GameController
 
         return bjack;
     }
-
-    // returns 0 for non-bust, 1 for bust
+    /**
+    * Method which takes the players hand value and sees if they have busted
+    *returns 0 for non-bust, 1 for bust 
+    */
     public int bustTest(Player hand)
     {
         int bust = 0;
@@ -72,7 +98,9 @@ public class GameController
         }
         return bust;
     }
-
+    /**
+    * Method which takes the players hand value and sees if they have beat the dealer 
+    */
     public Player checkWinner()
     {
         int userVal = user.getHandValue();
